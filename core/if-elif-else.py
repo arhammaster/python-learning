1674e08f61b616b8021076aac4e0ccfbefdd49ac
arhammaster <arhammmaster@gmail.com>
2026-03-29 05:38:39 +0000

    changing initiall to macth name

--- core/if-elif-else.py
+++ core/if-elif-else.py
@@ -2,11 +2,11 @@ initial1 = input ("Whats your initial : ")
 
 if initial1 == 'A' :
   print("Good morning Arjun")
-elif initial1 == 'M' :
+elif initial1 == 'J' :
   print("Good morning Jack")
-elif initial1 == 'N' :
+elif initial1 == 'S' :
   print("Good morning Sarah") 
-elif initial1 == 'a' :
+elif initial1 == 'j' :
   print("Good morning Jill")
 else :
   print("I don't know you")
