b40b1cda2b53b83db92a854207212e319e52051e
Arham Master <arhammmaster@gmail.com>
2026-03-30 17:54:26 -0400

    Update Person class constructor to include new_number parameter

--- oop/telephone/person.py
+++ oop/telephone/person.py
@@ -1,6 +1,6 @@
 class Person :
 
-    def __init__(self, name:str, number:int):
+    def __init__(self, number:int, name:str, new_number:int):
         self.name:str = name
         self.number:int = number
-        
+        self.new_number:int = number
